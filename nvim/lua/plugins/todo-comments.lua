df57ab8db82b60f6b32985edbcb281f96e23cb2f
cvcvka5 <cvcvka5@gmail.com>
2026-01-13 16:11:30 +0100

    added better motion support with flash + extra extensions

---
+++ nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+-- WARNING: hey
+-- TODO: yeah
+-- HACK: this HACK
+-- PERF: fully optimized
+-- FIX: fix this
+-- NOTE: continue
+-- WARNING: ???
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {}, -- this triggers setup({})
+}
+
