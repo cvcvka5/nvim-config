1909db0d25230d7585c3c658cb9d0114bceee3e8
cvcvka5 <cvcvka5@gmail.com>
2026-01-14 20:52:18 +0100

    deleted lualine and added some more shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,7 @@ vim.opt.termguicolors = true
 vim.keymap.set("n", "<leader>e", ":Ex<CR>")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("n", "<leader>r", ":w<CR>:!python3 %<CR>")
 
 
 -- ============================
